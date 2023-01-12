fa01f1dceccca8568b82e8394975b71ee89f87e2
Rodrigo NS <rodrigonovoa97s@gmail.com>
2023-01-12 20:49:02 +0100

    adding glide library to load images from urls and styling book list items

--- app/src/main/java/app/rodrigonovoa/mybookcollection/api/FakeGoogleBooksApi.kt
+++ app/src/main/java/app/rodrigonovoa/mybookcollection/api/FakeGoogleBooksApi.kt
@@ -4,8 +4,8 @@ import app.rodrigonovoa.mybookcollection.data.Book
 
 class FakeGoogleBooksApi: GoogleBooksApiInterface {
     override fun getBooksFromApi(): List<Book> {
-        val book1 = Book(1, "The Dunwich Horror", "H.P. Lovecraft", "Example book numer one", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.es%2FDunwich-Horror-Classics-Go-English-ebook%2Fdp%2FB077V2V364&psig=AOvVaw3nE7R_RRVcTn1rEmLqMmab&ust=1672865124483000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCIDlxbeirPwCFQAAAAAdAAAAABAE")
-        val book2 = Book(1, "Final Empire", "Brandon Sanderson","Example book numer two", "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.kobo.com%2Fes%2Fes%2Faudiobook%2Fthe-final-empire-2&psig=AOvVaw0I1u-UMSWIf7y3Qrz__s6f&ust=1672865278726000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPjCjIGjrPwCFQAAAAAdAAAAABAJ")
+        val book1 = Book(1, "The Dunwich Horror", "H.P. Lovecraft", "Example book numer one", "https://m.media-amazon.com/images/I/41mIf-FO3EL.jpg")
+        val book2 = Book(1, "Final Empire", "Brandon Sanderson","Example book numer two", "https://kbimages1-a.akamaihd.net/064d08d0-4048-439c-9e88-479b0ac9ce57/353/569/90/False/the-final-empire-2.jpg")
         val book3 = Book(1, "Misery", "Stephen King","Example book numer one", "https://m.media-amazon.com/images/I/51SSlC2WctL.jpg")
 
         return listOf(book1, book2, book3)
--- app/src/main/java/app/rodrigonovoa/mybookcollection/ui/myBooks/MyBooksListAdapter.kt
+++ app/src/main/java/app/rodrigonovoa/mybookcollection/ui/myBooks/MyBooksListAdapter.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import app.rodrigonovoa.mybookcollection.R
 import app.rodrigonovoa.mybookcollection.data.Book
+import com.bumptech.glide.Glide
 
 class MyBooksListAdapter(private val booksList: List<Book>) : RecyclerView.Adapter<MyBooksListAdapter.ViewHolder>() {
 
@@ -20,8 +21,19 @@ class MyBooksListAdapter(private val booksList: List<Book>) : RecyclerView.Adapt
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val bookItem = booksList[position]
-        // holder.imvCover.setImageResource(ItemsViewModel.image)
+
+        loadCover(holder, bookItem)
         holder.tvTitle.text = bookItem.name
+        holder.tvAuthor.text = bookItem.author
+    }
+
+    private fun loadCover(
+        holder: ViewHolder,
+        bookItem: Book
+    ) {
+        Glide.with(holder.imvCover.context)
+            .load(bookItem.imageUrl)
+            .into(holder.imvCover);
     }
 
     override fun getItemCount(): Int {
@@ -31,5 +43,6 @@ class MyBooksListAdapter(private val booksList: List<Book>) : RecyclerView.Adapt
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         val imvCover: ImageView = itemView.findViewById(R.id.imv_book_cover)
         val tvTitle: TextView = itemView.findViewById(R.id.tv_book_name)
+        val tvAuthor: TextView = itemView.findViewById(R.id.tv_book_author)
     }
 }
